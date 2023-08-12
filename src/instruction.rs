1e01743866b238ae8b1fa60c2f05dd1db7aec9c7
virtualdesigner <smvirtualdesigner@gmail.com>
2023-08-12 11:08:49 +0530

    Add basic operations

--- src/instruction.rs
+++ src/instruction.rs
@@ -2,7 +2,12 @@
 #[derive(Debug)]
 pub enum Opcode {
     HLT,
-    IGL
+    IGL,
+    LOAD,
+    ADD,
+    SUB,
+    MUL,
+    DIV
 }
 
 pub struct Instruction {
@@ -21,6 +26,11 @@ impl From<u8> for Opcode {
     fn from(byte: u8) -> Self {
         match byte {
             0 => return Opcode::HLT,
+            1 => return Opcode::LOAD,
+            2 => return Opcode::ADD,
+            3 => return Opcode::SUB,
+            4 => return Opcode::MUL,
+            5 => return Opcode::DIV,
             _ => return Opcode::IGL
         }
     }
--- src/vm.rs
+++ src/vm.rs
@@ -2,7 +2,8 @@ use crate::instruction::Opcode;
 pub struct VM {
     registers: [i32; 32],
     pc: usize,
-    program: Vec<u8>
+    program: Vec<u8>,
+    remainder: u32,
 }
 
 impl VM {
@@ -10,32 +11,87 @@ impl VM {
         VM {
             registers: [0; 32],
             pc: 0,
-            program: vec![]
+            program: vec![],
+            remainder: 0,
         }
     }
 
     pub fn run(&mut self) {
-        loop {
-            if self.pc >= self.program.len() {
-                break;
+        let mut is_done = false;
+        while !is_done {
+            is_done = self.execute_instruction();
+        }
+    }
+
+    pub fn run_once(&mut self) {
+        self.execute_instruction();
+    }
+
+    pub fn execute_instruction(&mut self) -> bool {
+        if self.pc >= self.program.len() {
+            return true;
+        }
+
+        match self.decode_opcode() {
+            Opcode::HLT => {
+                println!("HLT Encountered!");
+                true
             }
-            match self.decode_opcode() {
-                Opcode::HLT => {
-                    println!("HLT Encountered!");
-                    return;
-                },
-                _ => {
-                    println!("Error: Unrecognized opcode! Terminating!");
-                    return;
-                }
+            Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits();
+                self.registers[register] = number as i32;
+                false
+            }
+            Opcode::ADD => {
+                let number_1 = self.registers[self.next_8_bits() as usize];
+                let number_2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = number_1 + number_2;
+                false
+            }
+            Opcode::SUB => {
+                let number_1 = self.registers[self.next_8_bits() as usize];
+                let number_2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = number_1 - number_2;
+                false
+            }
+            Opcode::MUL => {
+                let number_1 = self.registers[self.next_8_bits() as usize];
+                let number_2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = number_1 * number_2;
+                false
+            }
+            Opcode::DIV => {
+                let number_1 = self.registers[self.next_8_bits() as usize];
+                let number_2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = number_1 / number_2;
+                self.remainder = (number_1 % number_2) as u32;
+                false
+            }
+            other => {
+                println!("Error: Unrecognized opcode {:?}! Terminating!", other);
+                return true;
             }
         }
     }
 
-    pub fn decode_opcode(&mut self) -> Opcode {
+    fn decode_opcode(&mut self) -> Opcode {
         let opcode = Opcode::from(self.program[self.pc]);
         self.pc += 1;
         return opcode;
+    }
+
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.pc];
+        self.pc += 1;
+        return result;
+    }
+
+    fn next_16_bits(&mut self) -> u16 {
+        let first_part = (self.program[self.pc] as u16) << 8;
+        let second_part = self.program[self.pc + 1] as u16;
+        self.pc += 2;
+        return first_part | second_part;
     }
 }
 
@@ -65,5 +121,13 @@ mod tests {
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = VM::new();
+        test_vm.program = vec![1, 0, 1, 244];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
     }
 }
